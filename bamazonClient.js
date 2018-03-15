3a3a9fd28ee0b23f16b09df586e46eb03e1af721
catalyticstorm <32317951+catalyticstorm@users.noreply.github.com>
2018-03-14 21:13:31 -0400

    Update client

--- bamazonClient.js
+++ bamazonClient.js
@@ -17,65 +17,62 @@ var connection = mysql.createConnection({
 	user: "root",
 
 	// Your password
-	password: "PUT PASSWORD HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+	password: "Hypercam2",
 	database: "bamazon"
 });
 
 connection.connect(function(err) {
-	if (err) {
-		console.log("An error occured. Ending...");
-		connection.destroy();
-		console.log("Any connections have been terminated.");
-		
-	}
+	if (err) throw err;
 	console.log("Connected!");
-	lookUpItem();
+	connection.query("SELECT * FROM products", function(err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		for (var i = 0; i < result.length; i++) {
+			console.log("SKU: " + result[i].item_id + " | PDCT: " + result[i].product_name + " | DPT: " + result[i].department_name + " | PC: $" + result[i].price + " | QTY AVAIL: " + result[i].stock_quantity);
+			productList.push(result[i].product_name);
+		}
+//		connection.release();
+	});
+	;
 });
 
-function lookUpItem(answer) {
-	var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
-	
-	connection.query(query, { product_name: answer.product_name }, function(err, res) {
-		for (var i = 0; i < res.length; i++) {
-			console.log("SKU: " + res[i].item_id + " | PDCT: " + res[i].product_name + " | DPT: " + res[i].department_name + " | PC: $" + res[i].price + " | QTY AVAIL: " + res[i].stock_quantity);
-			productList.push(res[i].product_name);
-		}
-		runSearch();
-	});
-	
-	inquirer.prompt({
-		name: "action",
-		type: "list",
-		message: "What do you want to buy?",
-		choices: productList
-	})
-	.then(function(answer) {
-		buyItem();
-	});
-}
+// item_id, product_name, department_name, price, stock_quantity
 
-function buyItem() {
-	inquirer.prompt({
-		name: "buyAmt",
-		type: "input",
-		message: "How many do you want to buy? NUMBER ONLY"
-	})
-	.then(function(err) {
-		if (err) {
-			console.log("Error in Item Purchase. Terminating connection");
-			connection.destroy();
-			console.log("");
-			
-		}
-		console.log("You want " + user.buyAmt + " " + "PLACE ITEM TAG HERE");
-		console.log("");
-		console.log("Placing order...");
-		console.log("");
-		console.log("Done! But I don't like it---")
-		console.log("You have " + user.buyAmt + " " + "PLACE ITEM TAG HERE" + " coming to su casa.");
-	})
-}
-
-connection.end(function(err) {
-	console.log("Connection Ended. Thanks for fake-shopping with us.")
-});
+//function lookUpItem(result) {
+//	getItems();
+//	inquirer.prompt({
+//		name: "action",
+//		type: "list",
+//		message: "What do you want to buy?",
+//		choices: productList
+//	})
+//	.then(function(result) {
+//		buyItem();
+//	});
+//}
+//
+//function buyItem() {
+//	inquirer.prompt({
+//		name: "buyAmt",
+//		type: "input",
+//		message: "How many do you want to buy? NUMBER ONLY"
+//	})
+//	.then(function(err) {
+//		if (err) {
+//			console.log("Error in Item Purchase. Terminating connection");
+//			connection.destroy();
+//			console.log("");
+//			
+//		}
+//		console.log("You want " + user.buyAmt + " " + "PLACE ITEM TAG HERE");
+//		console.log("");
+//		console.log("Placing order...");
+//		console.log("");
+//		console.log("Done! But I don't like it---")
+//		console.log("You have " + user.buyAmt + " " + "PLACE ITEM TAG HERE" + " coming to su casa.");
+//	})
+//}
+//getItems();
+//connection.end(function(err) {
+//	console.log("Connection Ended. Thanks for fake-shopping with us.")
+//});
